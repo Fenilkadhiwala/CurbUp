b754284df46ebff10aa1008848999e0829c377e0
Fenil_Kadhiwala <79686889+Fenilkadhiwala@users.noreply.github.com>
2026-04-04 16:53:51 -0400

    feat:complete signup auth flow with auth0 (#3)
    Completed the authentication with auth0

--- UI/babel.config.js
+++ UI/babel.config.js
@@ -19,7 +19,6 @@ module.exports = function (api) {
           },
         },
       ],
-
       ["module:react-native-dotenv"],
     ],
   };
---
+++ UI/src/screens/SigninScreen.tsx
@@ -0,0 +1,5 @@
+import { SigninForm } from "@/src/components/SigninForm";
+
+export default function SigninScreen() {
+  return <SigninForm />;
+}
